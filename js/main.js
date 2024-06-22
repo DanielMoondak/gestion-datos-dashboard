685f1a09745a9d4d9d0d08cdf768946c6dad006f
DanielMoondak <danielruperto26@gmail.com>
2024-06-22 17:49:18 -0600

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Aquí puedes agregar lógica JavaScript necesaria para tu aplicación
-// Por ejemplo, configuración de gráficos con AmCharts
-am4core.ready(function() {
-    // Ejemplo de configuración de gráfico con AmCharts
-    // Aquí iría el código para crear y configurar el gráfico
-});
